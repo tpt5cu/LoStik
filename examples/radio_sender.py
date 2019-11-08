328462a5b05fefbfc8cfdda58aac8e6fa696d2ca
Tuomas Talvitie <ttalvitie49@gmail.com>
2019-11-08 12:42:13 -0500

    simple counter val + hex func

--- examples/radio_sender.py
+++ examples/radio_sender.py
@@ -35,12 +35,15 @@ class PrintLines(LineReader):
             print(exc)
         print("port closed")
 
-    def tx(self):
+    def tx(self, val):
         self.send_cmd("sys set pindig GPIO11 1")
         #Convert to hexadecimal 
-        txmsg = 'radio tx %x%x' % (int(time.time()), self.frame_count)
+        # txmsg = 'radio tx %x%x' % (int(time.time()), self.frame_count)
         #Hexadecimal for 'hello'
-        txmsg = 'radio tx 68656c6c6f'
+        # txmsg = 'radio tx 68656c6c6f'
+        val = str(val)
+        val = val.encode('hex')
+        txmsg = 'radio tx %s' % val
         self.send_cmd(txmsg)
         time.sleep(.3)
         self.send_cmd("sys set pindig GPIO11 0")
@@ -54,6 +57,8 @@ class PrintLines(LineReader):
 
 ser = serial.Serial(args.port, baudrate=57600)
 with ReaderThread(ser, PrintLines) as protocol:
+    val = 0
     while(1):
-        protocol.tx()
+        val +=1
+        protocol.tx(val)
         time.sleep(10)
